c5fc51d8a445b071b24a3eaa6d9cab46d7dc643c
ThianDhaene <90450781+ThianDhaene@users.noreply.github.com>
2023-04-27 12:38:17 +0200

    -aanmaken van programma voor 2de microcontroller

---
+++ C-MCU2/C-MCU2/main.c
@@ -0,0 +1,18 @@
+/*
+ * C-MCU2.c
+ *
+ * Created: 27/04/2023 12:37:02
+ * Author : D'haeneThian
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
